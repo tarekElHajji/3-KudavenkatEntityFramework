cdfe50e6dcd51996678416ec19f6e5b3b7575f7a
tarekElHajji <tarek.hajji.93@gmail.com>
2018-12-03 23:42:17 +0100

    "add customHtmlHelpers image, declare it in the web.config and change the details view to display the image"

---
+++ Venkat/Venkat/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Venkat.CustomHtmlHelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            tb.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
