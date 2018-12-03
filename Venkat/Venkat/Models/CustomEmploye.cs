4b56f5d432b3430d7f22d73eaa0ad4305fb19e68
tarekElHajji <tarek.hajji.93@gmail.com>
2018-12-03 22:38:51 +0100

    "add customEmployee and customDepartemtn classes and change default routeConfig"

---
+++ Venkat/Venkat/Models/CustomEmploye.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Venkat.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employe
+    {
+    }
+
+    public class EmployeeMetaData
+    {
+        [Required]
+        public int IdEmployee { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+        [Required]
+        public string UrlImage { get; set; }
+        [Required]
+        public string AltImage { get; set; }
+
+        [Display(Name = "Department")]
+        [Required]
+        public int DepartmentId { get; set; }
+
+
+    }
+}
